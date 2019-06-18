fcff718755c7e0aad8effe32f9ec25d1df7d7043
js-tut <johnsmilga@gmail.com>
2019-06-17 18:20:34 -0700

    5-layout-component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
 import React from "react"
-
+import Layout from "../components/layout"
 const Error = () => {
   return (
-    <div>
+    <Layout>
       <h1>This is our Error Page</h1>
-    </div>
+    </Layout>
   )
 }
 
