75828fc8b26abb0629f02daf17b423b04f597b14
js-tut <johnsmilga@gmail.com>
2019-06-21 12:53:25 -0700

    16-component-setup

---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PostCard from "./PostCard"
+const PostList = () => {
+  return (
+    <div>
+      hello from post list
+      <PostCard />
+    </div>
+  )
+}
+
+export default PostList
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
+import PostList from "../components/PostList"
 export default () => (
   <Layout>
-    Hello world!
-    <div>
-      <Link to="/post/">Post Page</Link>
-    </div>
+    <PostList />
   </Layout>
 )
