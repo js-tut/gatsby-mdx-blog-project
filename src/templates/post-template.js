26395d3009adb53de56db3639af9f6ec9fed9f2a
js-tut <johnsmilga@gmail.com>
2019-06-21 17:34:32 -0700

    21-post-template

---
+++ src/templates/post-template.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "../css/postTemplate.module.css"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
+import Layout from "../components/layout"
+import { MDXRenderer } from "gatsby-mdx"
+const postTemplate = () => {
+  return <div>this is a post template</div>
+}
+
+export default postTemplate
